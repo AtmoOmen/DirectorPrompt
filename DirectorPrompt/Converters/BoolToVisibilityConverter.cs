84d4d75a341e09f10bf1e74bd193d905cb241a49
AtmoOmen <atmoomen2004@outlook.com>
2026-07-11 19:08:43 +0800

    简化转换器

--- DirectorPrompt/Converters/BoolToVisibilityConverter.cs
+++ DirectorPrompt/Converters/BoolToVisibilityConverter.cs
@@ -6,22 +6,23 @@ namespace DirectorPrompt.Converters;
 
 public sealed class BoolToVisibilityConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        var boolValue = value is bool b       && b;
-        var invert    = parameter is string s && s == "Invert";
+        var boolValue = value is true;
+        var invert    = parameter is "Invert";
 
         return boolValue ^ invert ?
                    Visibility.Visible :
                    Visibility.Collapsed;
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         var visibility = value is Visibility v ?
                              v :
                              Visibility.Collapsed;
-        var invert = parameter is string s && s == "Invert";
+
+        var invert = parameter is "Invert";
 
         return (visibility == Visibility.Visible) ^ invert;
     }
