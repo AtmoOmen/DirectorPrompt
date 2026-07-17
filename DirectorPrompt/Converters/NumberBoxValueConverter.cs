92bb23a51c1d1bd8034e0fb42e055cd60778038a
AtmoOmen <atmoomen2004@outlook.com>
2026-07-18 03:57:02 +0800

    修复输入框

--- DirectorPrompt/Converters/NumberBoxValueConverter.cs
+++ DirectorPrompt/Converters/NumberBoxValueConverter.cs
@@ -10,13 +10,19 @@ public sealed class NumberBoxValueConverter : IValueConverter
 
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        return value is null ? null : System.Convert.ToDouble(value, culture);
+        return value is null ? double.NaN : System.Convert.ToDouble(value, culture);
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is null)
             return BindingOperations.DoNothing;
+
+        if (value is double d && double.IsNaN(d))
+        {
+            var isNullable = Nullable.GetUnderlyingType(targetType) != null;
+            return isNullable ? null : BindingOperations.DoNothing;
+        }
 
         var type = Nullable.GetUnderlyingType(targetType) ?? targetType;
         return System.Convert.ChangeType(value, type, culture);
