c795c56346d879c1111b837a1b6bc0ea14089c82
AtmoOmen <atmoomen2004@outlook.com>
2026-07-20 14:35:16 +0800

    修复样式继承

--- DirectorPrompt/Views/Components/RemoveButton.axaml.cs
+++ DirectorPrompt/Views/Components/RemoveButton.axaml.cs
@@ -1,9 +1,12 @@
+using System;
 using Avalonia.Controls;
 
 namespace DirectorPrompt.Views.Components;
 
 public partial class RemoveButton : Button
 {
+    protected override Type StyleKeyOverride => typeof(Button);
+
     public RemoveButton()
     {
         InitializeComponent();
